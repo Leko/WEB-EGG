32cf1f60ece5a9efaba386a3dea26b69579346c7
Leko <leko.noor@gmail.com>
2017-06-23 23:55:00 +0900

    chore: Disable calendar

--- config.rb
+++ config.rb
@@ -24,25 +24,26 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "post/{title}/index.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
+  blog.sources = "post/{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tag/{tag}.html"
+  blog.layout = "layout/article"
+  blog.summary_generator = Proc.new do |article, rendered|
+    rendered.split('<!--more-->').first
+  end
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".md"
 
   blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  blog.paginate = true
+  blog.per_page = 15
+  blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false
