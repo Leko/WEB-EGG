206256a0a642f122b19e55f1d3fc5576c027bd2c
Leko <leko.noor@gmail.com>
2017-07-09 17:28:35 +0900

    fix: Bring init process to main.js

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,10 +1,28 @@
 // @flow
-import LazyLoadApp from './LazyLoadApp'
 import SearchApp from './SearchApp'
+import LazyLoadApp from './LazyLoadApp'
 import { registerWorkers } from './ServiceWorker'
 import swPrecacheConfig from '../../sw-precache-config'
 
+// --- Init SearchApp
+const dispatcher = new Dispatcher()
+const appContext = new Context({
+  dispatcher,
+  store: AppStoreGroup.create()
+})
+
+if (process.env.NODE_ENV !== 'production') {
+  const ContextLogger = require('almin-logger')
+  const logger = new ContextLogger()
+  logger.startLogging(appContext)
+}
+
+ReactDOM.render(<Root appContext={appContext} />, document.getElementById('search'))
+
+// --- Init LazyLoadApp
+LazyLoadApp(document.querySelectorAll('.markdown img'))
+
+// --- Init ServiceWorker
 registerWorkers([
   swPrecacheConfig.swFile
 ])
-LazyLoadApp(document.querySelectorAll('.markdown img'))
