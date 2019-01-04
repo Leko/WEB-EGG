112cb996087054c45dcf0cfe50832450f01ab60c
Leko <leko.noor@gmail.com>
2019-01-05 00:15:08 +0900

    refactor: split to <Brand />

---
+++ src/components/Brand.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { rhythm, scale } from '../utils/typography'
+
+export function Brand({ title, dimmed = false } = {}) {
+  if (dimmed) {
+    return (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+          marginBottom: rhythm(-1),
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+  return (
+    <h1
+      style={{
+        ...scale(1.5),
+        marginBottom: rhythm(1.5),
+        marginTop: 0,
+      }}
+    >
+      <Link
+        style={{
+          boxShadow: `none`,
+          textDecoration: `none`,
+          color: `inherit`,
+        }}
+        to={`/`}
+      >
+        {title}
+      </Link>
+    </h1>
+  )
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,57 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { Copyright } from './Copyright'
-
+import { Brand } from './Brand'
 import { rhythm, scale } from '../utils/typography'
 
 class Layout extends React.Component {
   render() {
     const { location, title, children, headerDimmed = false } = this.props
-    let header
 
-    if (!headerDimmed) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
     return (
       <div
         style={{
@@ -61,7 +17,7 @@ class Layout extends React.Component {
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
-        {header}
+        <Brand title={title} dimmed={headerDimmed} />
         {children}
         <Copyright />
       </div>
