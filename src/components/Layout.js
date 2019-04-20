4156ee300a7e1ac7bcd843808a8549474d6ca7a6
Leko <leko.noor@gmail.com>
2019-04-20 17:20:38 +0900

    refactor: Make Layout FC

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,25 +3,23 @@ import { Copyright } from './Copyright'
 import { Brand } from './Brand'
 import { rhythm } from '../utils/typography'
 
-class Layout extends React.Component {
-  render() {
-    const { title, children, headerDimmed = false } = this.props
+function Layout(props) {
+  const { title, children, headerDimmed = false } = props
 
-    return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: 700,
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <Brand title={title} dimmed={headerDimmed} />
-        {children}
-        <Copyright />
-      </div>
-    )
-  }
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: 700,
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      <Brand title={title} dimmed={headerDimmed} />
+      {children}
+      <Copyright />
+    </div>
+  )
 }
 
 export default Layout
