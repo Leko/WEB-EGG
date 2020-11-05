ee6e12decf0c4b1c58037c22ed9b1eafed501010
Leko <leko.noor@gmail.com>
2020-11-06 02:36:40 +0800

    Add initial value

--- src/utils/color.js
+++ src/utils/color.js
@@ -2,7 +2,12 @@ const key = 'web-egg-prefers-color-scheme'
 const windowGlobal = (typeof window !== 'undefined' && window) || null
 
 export function getPrefersColorScheme() {
-  return windowGlobal?.localStorage.getItem(key)
+  return (
+    windowGlobal?.localStorage.getItem(key) ??
+    (windowGlobal?.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light')
+  )
 }
 
 export function setPrefersColorScheme(theme) {
