71a1d64ee93bdfae037a529b56bffe0f400af5d1
Leko <leko.noor@gmail.com>
2020-11-06 01:04:24 +0800

    Fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,18 @@
 import React from 'react'
-
+import Root from '../components/Root'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
 class NotFoundPage extends React.Component {
   render() {
     return (
-      <Layout location={this.props.location}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
+      <Root>
+        <Layout location={this.props.location}>
+          <SEO title="404: Not Found" />
+          <h1>Not Found</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </Layout>
+      </Root>
     )
   }
 }
